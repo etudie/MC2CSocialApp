de51970e9c170c0ac0702e565464a045d66f04c4
xue_hua <xue_hua@hermanmiller.com>
2021-09-06 11:51:13 -0400

    Add React Client

---
+++ client/src/components/layouts/Landing.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <section className='landing'>
+      <div className='dark-overlay'>
+        <div className='landing-inner'>
+          <h1 className='x-large'>MC2C Social</h1>
+          <p className='lead'>
+            Create a profile to connect, share posts and get help from other mentees
+          </p>
+          <div className='buttons'>
+            <a href='register.html' className='btn btn-primary'>
+              Sign Up
+            </a>
+            <a href='login.html' className='btn btn-light'>
+              Login
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Landing;
